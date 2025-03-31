e09858fdc13409aee7791f6de849d9e39b088814
Kartik Manchanda <kartik1182.becse24@chitkara.edu.in>
2025-03-31 19:27:30 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,12 @@
+int isPrime(int num){
+    if(num <= 1) return 0;
+    if(num == 2) return 1;
+    
+    for(int i = 2; i < num; i++){
+        
+        if(num%i ==0){
+            return 0;
+        }
+        return 1;
+    }
+}
