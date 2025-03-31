6b0d53d6e5531ea90eb2cdaf21d61ea178609539
Kartik Manchanda <kartik1182.becse24@chitkara.edu.in>
2025-03-31 19:02:17 +0530

    Solution to Bubble Sort Implementation

---
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -0,0 +1,13 @@
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int &arr[], int n){
+    for(int i = 1; i <= n; i++){
+        if(arr[i-1] > arr[i]){
+            swap(arr[i-1], arr[i]);
+        }
+    }
+}
