91067b04640143e6f6c0be622c8c3c8fe8dadb3e
Kartik Manchanda <kartik1182.becse24@chitkara.edu.in>
2025-03-31 20:53:19 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,14 +1,35 @@
-int findUnsortedSubarray(int arr[], int n){
-    int i = 0, j = n-1;
-    int left = 0, right = 0;
-    while(i<j){
-        if(arr[i] > arr[i+1]){
+int findUnsortedSubarray(int arr[], int n) {
+    int left = -1, right = -1;
+
+    // Find left boundary
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
             left = i;
+            break;
         }
-        else if(arr[j-1] > arr[j]){
-            right = j;
-        }
-        i++, j--;
     }
-    return right-left-1;
+    if (left == -1) return 0; // Already sorted
+
+    // Find right boundary
+    for (int j = n - 1; j > 0; j--) {
+        if (arr[j] < arr[j - 1]) {
+            right = j;
+            break;
+        }
+    }
+
+    // Find min and max in the unsorted window
+    int minVal = arr[left], maxVal = arr[left];
+    for (int k = left; k <= right; k++) {
+        if (arr[k] < minVal) minVal = arr[k];
+        if (arr[k] > maxVal) maxVal = arr[k];
+    }
+
+    // Expand left boundary
+    while (left > 0 && arr[left - 1] > minVal) left--;
+
+    // Expand right boundary
+    while (right < n - 1 && arr[right + 1] < maxVal) right++;
+
+    return right - left + 1;
 }
